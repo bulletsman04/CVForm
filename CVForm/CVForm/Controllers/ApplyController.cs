77f4eebdfe3036981ce38cedf2070b0c24244b94
bulletsman04 <bulletsman04@gmail.com>
2018-11-13 20:00:02 +0100

    Apply form (controller + views) - basic version. Additionaly job application list in job offer detials and companies controller & view #16 #12

---
+++ CVForm/CVForm/Controllers/ApplyController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CVForm.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVForm.Controllers
+{
+    public class ApplyController : Controller
+    {
+        public async Task<ActionResult> Index(int id)
+        {
+            var jobApplication = new JobApplication()
+            {
+                OfferId = id
+            };
+            return View(jobApplication);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(JobApplication model)
+        {
+            if (!ModelState.IsValid)
+            {
+               return View(model);
+            }
+
+            var id = JobOfferController._jobOffers[model.OfferId-1].JobApplications.Count == 0? 0 : JobOfferController._jobOffers[model.OfferId-1].JobApplications.Max(j => j.Id) + 1;
+
+            JobOfferController._jobOffers[model.OfferId-1].JobApplications.Add(new JobApplication()
+            {
+                Id = id,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                EmailAddress = model.EmailAddress,
+                PhoneNumber = model.PhoneNumber,
+                ContactAgreement = model.ContactAgreement,
+                OfferId = model.OfferId
+            });
+
+            return RedirectToAction("Details","JobOffer",new {id = model.OfferId});
+        }
+
+        public async Task<IActionResult> Details(int offerId, int applicationId)
+        {
+            var application = JobOfferController._jobOffers[offerId - 1].JobApplications[applicationId];
+
+            return View(application);
+        }
+    }
+}
---
+++ CVForm/CVForm/Controllers/CompaniesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using CVForm.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVForm.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(JobOfferController._companies);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var offer = JobOfferController._companies.Find(item => item.ID == id);
+
+            if (offer == null)
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+
+            return View(offer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Company model)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            var offer = JobOfferController._companies.Find(item => item.ID == model.ID);
+            offer.Name = model.Name;
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            JobOfferController._companies.RemoveAll(item => item.ID == id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
