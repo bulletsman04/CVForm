2f7d4481e01add1d5445b207dbbf57550025fab2
bulletsman04 <bulletsman04@gmail.com>
2019-01-11 08:51:51 +0100

    CV file is required

--- CVForm/CVForm/Models/JobApplication/JobApplicationViewModel.cs
+++ CVForm/CVForm/Models/JobApplication/JobApplicationViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -8,6 +9,7 @@ namespace CVForm.Models
 {
     public class JobApplicationViewModel: JobApplication
     {
+        [Required]
         public IFormFile CvFile { get; set; }
 
     }
